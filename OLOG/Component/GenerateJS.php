515c6a6f36190767e50ad420253ae6299de975b4
Oleg Loginov <olognv@gmail.com>
2016-03-29 18:21:07 +0300

    1

---
+++ OLOG/Component/GenerateJS.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace OLOG\Component;
+
+class GenerateJS
+{
+    public static function buildJavascriptForSportbox2015()
+    {
+        $components_js_arr = array();
+
+        $components_arr = \OLOG\ConfWrapper::value('component_classes_arr', []);
+        foreach ($components_arr as $component_class_name) {
+            self::registerComponentJs($component_class_name, $components_js_arr);
+        }
+
+        $js_arr = array(
+        );
+
+        $source_js_arr = array_merge($js_arr, $components_js_arr);
+
+        self::generateJS(
+            $source_js_arr,
+            './assets/common.js'
+        );
+    }
+
+    public static function registerComponentJs($class_name, &$js_arr){
+        //\Sportbox\Helpers::assert($class_name instanceof \Sportbox\Component\InterfaceComponent, $class_name . ' must implement');
+
+        // TODO: check interface
+
+        $js_path = $class_name::getJsPath();
+
+        array_push($js_arr, $js_path);
+        return $js_arr;
+    }
+
+    /**
+     * сборщик агрегата javascript
+     * @param $javascripts_arr - массив склеиваемых скриптов
+     * @param $output_path - путь к агрегату
+     */
+    public static function generateJS($javascripts_arr, $output_path)
+    {
+        $js_base_path = __DIR__ . '/../..';
+        $contents = '';
+
+        foreach ($javascripts_arr as $javascript) {
+            $file_path = $javascript;
+
+            // путь к скрипту не начинается с / или x:\
+            if (!preg_match('@(^\/|^[a-z]:[\/\\\\])@i', $javascript)){
+                $file_path = $js_base_path . '/' . $javascript;
+            }
+
+            $contents .= file_get_contents($file_path);
+            $contents .= "\n";
+        }
+
+        $output_path = $js_base_path . '/' . $output_path;
+        file_put_contents($output_path, $contents);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -7,33 +7,4 @@ require_once '../vendor/autoload.php';
 \OLOG\ConfWrapper::assignConfig(\PHPComponentsDemo\Config::get());
 
 \OLOG\Component\GenerateCSS::generateCss();
-
-/*
-echo '<div>MODELS <a href="/?a=add_model">+</a></div>';
-
-// ACTIONS
-
-if (isset($_GET['a'])){
-    if ($_GET['a'] == 'add_model'){
-        $new_model = new \PHPModelDemo\DemoModel();
-        $new_model->setTitle(rand(1, 1000));
-        $new_model->save();
-    }
-}
-
-// DISPLAY
-
-$models_ids_arr = \OLOG\DB\DBWrapper::readColumn(
-    \PHPModelDemo\Config::DB_NAME_PHPMODELDEMO,
-    'select id from ' . \PHPModelDemo\DemoModel::DB_TABLE_NAME . ' order by id desc'
-);
-
-echo '<ul>';
-
-foreach ($models_ids_arr as $model_id){
-    $model_obj = \PHPModelDemo\DemoModel::factory($model_id);
-    echo '<div>' . $model_obj->getTitle() . '</div>';
-}
-
-echo '</ul>';
-*/
+\OLOG\Component\GenerateJS::generateJS();
